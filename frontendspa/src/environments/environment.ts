1208faf1b9818fca76f86ab03f775afa755b62dd
Álvaro LLancavil <alvllnkvl@gmail.com>
2025-04-02 16:23:10 -0300

    Update environment.ts

--- frontendspa/src/environments/environment.ts
+++ frontendspa/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {};
 export const api = {
-    host: "http://ec2-13-217-166-11.compute-1.amazonaws.com/api",
+    host: "https://sigipro.app-dev.click/api",
     persona: "/persona",
     personas: "/personas",
     contrato: "/contrato",
